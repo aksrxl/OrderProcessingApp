a2302ea53850e5834661e018c5db490a4ede2ad1
aksrxl <aksrxl@gmail.com>
2020-10-03 17:13:42 +0530

    Added Home and Order Controller Test cases

---
+++ OrderProcessingApp/OrderProcessingApp.UnitTest/Controller/HomeControllerTest.cs
@@ -0,0 +1,22 @@
+﻿using OrderProcessingApp.Controllers;
+using Xunit;
+
+namespace OrderProcessingApp.UnitTest.Controller
+{
+    [Collection("HomeControllerTest")]
+    public class HomeControllerTest
+    {
+
+        [Fact]
+        public void Calling_Index_Returns_View()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+            // Act
+            var result = controller.Index();
+            // Assert
+            Assert.NotNull(result);
+        }
+
+    }
+}
